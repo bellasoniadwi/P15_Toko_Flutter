800584546941bc4e226970f76101b63d6bf547c4
bellasoniadwi <bellasoniadwi@gmail.com>
2022-12-01 15:49:19 +0700

    Upload Project Toko

---
+++ android/app/src/main/kotlin/com/example/pertemuan_15/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pertemuan_15
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/item.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Item{
+  String name;
+  int price;
+
+  Item({
+    required this.name, required this.price
+  });
+}
