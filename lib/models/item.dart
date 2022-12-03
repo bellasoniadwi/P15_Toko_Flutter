df5f158a383db9657eccd99417c77c1a42726ba4
bellasoniadwi <bellasoniadwi@gmail.com>
2022-12-03 12:57:57 +0700

    add 2 variable

--- lib/models/item.dart
+++ lib/models/item.dart
@@ -3,8 +3,13 @@ import 'package:flutter/material.dart';
 class Item{
   String name;
   int price;
+  String imageUrl;
+  int stock;
 
   Item({
-    required this.name, required this.price
+    required this.name, 
+    required this.price,
+    required this.imageUrl,
+    required this.stock
   });
 }
