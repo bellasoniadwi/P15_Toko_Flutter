442a6fc7d0fe6917771e19603866c5a4faa10b69
bellasoniadwi <bellasoniadwi@gmail.com>
2022-12-03 12:59:18 +0700

    pecah widget pada homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,43 +2,31 @@ import 'dart:html';
 
 import 'package:flutter/material.dart';
 import 'package:pertemuan_15/models/item.dart';
+import 'package:pertemuan_15/widgets/list_view.dart';
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatelessWidget {
+
   final List<Item> items = [
-    Item(name: 'Sugar', price:5000),
-    Item(name: 'Salt', price:2000),
+    Item(name: 'Bedcover', price: 300000, imageUrl: "bedcover.jpg", stock: 5),
+    Item(name: 'Boneka', price: 50000, imageUrl: "boneka.jpg", stock: 10),
+    Item(name: 'Sandal', price: 30000, imageUrl: "sandal.jpg", stock: 25),
+    Item(name: 'Tas', price: 40000, imageUrl: "tas.jpg", stock: 20)
   ];
+
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Shop'),
-        ),
-        body: Container(
-          margin: EdgeInsets.all(8),
-          child: ListView.builder(
-            padding: EdgeInsets.all(8),
-            itemCount: items.length,
-            itemBuilder: ((context, index){
-              final item = items[index];
-              return Card(
-                child: Container(
-                  margin: EdgeInsets.all(8),
-                  child: Row(
-                    children: [
-                      Expanded(child: Text(item.name)),
-                      Expanded(
-                        child: Text(item.price.toString(),
-                        textAlign: TextAlign.end,),
-                        ),
-                    ]),),
-              );
-            })), 
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Moo Shopping List'),
+        backgroundColor: Color.fromARGB(255, 243, 165, 191),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(8),
+        child: ListViews(
+          items: items,
+          page: '/item',
         ),
       ),
     );
-    throw UnimplementedError();
   }
 }
